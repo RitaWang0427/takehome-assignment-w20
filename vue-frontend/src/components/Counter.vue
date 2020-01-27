280204db254a64ac642a1473886917e142f74fd4
RitaWang0427 <55065578+RitaWang0427@users.noreply.github.com>
2020-01-27 15:45:43 -0500

    Update Counter.vue

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,17 +1,25 @@
 <template>
-  <p>I'm a Counter!</p>
+  <div id = "change">
+     <b-button v-on:click="count += 1">Add 1</b-button>
+     <b-button v-if="count > 0" v-on:click="count -= 1">Subtract 1</b-button>
+     <p>counter = {{count}}. </p>
+  </div>
 </template>
-
 <script>
 export default {
+  e1 : "change",
   data() {
-    return {
+    return{
       count: 0
     };
-  }
+  },
+  props :{
+    count: Number
+  },
 };
 </script>
 
 <style>
 </style>
+
 
